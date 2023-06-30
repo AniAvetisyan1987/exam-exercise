e0d1f2c139e2b237c7e6b2140148fbcc3c4b4b6c
Armen <Armen@DESKTOP-IRSKKA4>
2023-07-01 00:27:52 +0400

    Add project files.

---
+++ ConsoleApp1ffff/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1ffff
+{
+    struct Class1
+    {
+        public string[] myString;
+        public int[] myInt;
+        public Class1(int[] myArr)
+        {
+            myInt = myArr;
+            myString = new string[myArr.Length];
+            Console.WriteLine(myString.Length + " length");
+            makeStringArr();
+        }
+        public void makeStringArr()
+        {
+            for (int i=0;i<myInt.Length;i++)
+            {
+                Console.WriteLine(myInt[i].ToString());
+                myString[i] = myInt[i].ToString();
+            }
+            
+            for (int i=0; i<myString.Length;i++)
+            {
+                Console.WriteLine(myString[i]);
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1ffff/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp1ffff;
+
+internal class Program
+{
+   
+        private static void Main(string[] args)
+        {
+       
+        int[] arr = { 1, 2, 3, 4, 5 };
+        //Class1 clss = new Class1(arr);
+
+        int i = 1;
+        
+        ////////Recurcia//////////////
+        Console.WriteLine(SumRec(i));
+        Console.ReadKey();
+
+        }
+        static int SumRec(int i)
+        {
+            if (i == 100) {
+            return 1;
+        }
+        return i + SumRec(i + 1);
+    }
+
+}
